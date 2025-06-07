6645089f1f2b4accd8e165a3bbd6117176ab9b8a
AlejandroBlanco2001 <nuclearhands2001@gmail.com>
2025-06-07 01:30:45 -0500

    add(models): adding fields to the models

--- task/tasksmanagement/models/label.py
+++ task/tasksmanagement/models/label.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 """
 This model should have a name and owner. Add the necessary constraints to avoid duplicate values
 """
 class Label(models.Model):
-    pass
+    name = models.CharField(null=False, max_length=255)    
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
--- task/tasksmanagement/models/task.py
+++ task/tasksmanagement/models/task.py
@@ -1,7 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from label import Label
 
 '''
  Task: This model should have a title, description, completion status, owner, and a many-to-many relationship to Label.
 ''' 
 class Task(models.Model):
-    pass
+    title = models.CharField(null=False, max_length=255) 
+    description = models.TextField(null=False)
+    is_completed = models.BooleanField(default=False)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    label = models.ManyToManyField(Label, related_name='tasks')
